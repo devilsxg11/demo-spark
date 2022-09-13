66713086025089edf229dd0e6d4f0daf09df3530
songxiaoguang <songxiaoguang@gome.com.cn>
2022-09-13 19:38:39 +0800

    yarn

--- demo-spark-core/src/main/scala/com/demo/spark/core/wordcount/WordCount.scala
+++ demo-spark-core/src/main/scala/com/demo/spark/core/wordcount/WordCount.scala
@@ -16,18 +16,21 @@ object WordCount {
     //创建配置对象
     val conf: SparkConf = new SparkConf()
       .setAppName(s"${WordCount.getClass.getSimpleName}")
-      .setMaster("local[*]") //local[*] ：为当前 spark 作业分配当前计算机的可用 CPU core 的个数
+      .setMaster("yarn") //local[*] ：为当前 spark 作业分配当前计算机的可用 CPU core 的个数
     val sc = new SparkContext(conf)
 
     //生成RDD
-    val textPath = getClass.getClassLoader.getResource("word.txt").getPath
+    //val textPath = getClass.getClassLoader.getResource("word.txt").getPath
+    //val textPath = "hdfs:///tmp/sxg/word.txt";
+    val textPath = args(0);
+    println("----------------->" + textPath)
     val textRDD: RDD[String] = sc.textFile(textPath)
     //进行 transformation 操作和 action 操作
     val retRDD: RDD[(String, Int)] = textRDD.flatMap(line => line.split(" "))
       .map(word => (word, 1))
       .reduceByKey(_ + _)
     //调用 Java 工具类
-    print(DateUtil.getCurrentTime)
+    println(DateUtil.getCurrentTime)
     //将最终的数据进行输出
     retRDD.foreach(x => println(x._1 + ": " + x._2))
     //关闭资源
